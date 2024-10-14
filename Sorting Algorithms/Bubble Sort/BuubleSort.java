f0a1fab3135f2b9f8505053a45d1abc6b05a45b3
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-14 10:02:02 +0530

    BuubleSort

---
+++ Sorting Algorithms/Bubble Sort/BuubleSort.java
@@ -0,0 +1,66 @@
+import java.util.*;
+class BubbleSort{
+	static void sort(int arr[],int size){
+		   for(int i=0;i<size;i++){                                                                                                                                            
+			 for(int j=0;j<size-i-1;j++){          
+				 if(arr[j]>arr[j+1]){
+				 	int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				 
+				 
+				 
+				 }
+				
+			   }	
+		    }
+	
+	
+	
+	}
+
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter array size : ");
+		int size=sc.nextInt();
+		int arr[]=new int[size];
+		System.out.println("Enter array elements : ");
+		for(int i=0;i<size;i++){
+			arr[i]=sc.nextInt();
+		
+		}
+
+		System.out.print("Given Array : ");
+		for(int i=0;i<size;i++){                                                                                                                                          System.out.print(arr[i]+" ");                                                                                                                                                                                                                                                                                         	   }
+	       System.out.println( );
+	       System.out.print("Sorted Array : ");
+	       sort(arr,size);
+	        for(int i=0;i<size;i++){                                                                                                                                              System.out.print(arr[i]+" ");                                                                                                                                                                                                                                                                                             }                                                                                                                                                           
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
