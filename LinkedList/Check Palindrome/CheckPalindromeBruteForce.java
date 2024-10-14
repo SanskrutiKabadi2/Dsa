fe0c558e5d5938e0e41a206a0e490a34411b78a2
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-14 09:51:18 +0530

    Check Palindrome bruteforce

---
+++ LinkedList/Check Palindrome/CheckPalindromeBruteForce.java
@@ -0,0 +1,100 @@
+import java.util.*;
+class Node{
+	int data;
+	Node next;
+	Node(){
+		next=null;
+	}
+
+
+
+
+}
+class LinkedList{
+	Node head=null;
+
+	void createList(int data){
+		Node p=new Node();
+		p.data=data;
+		if(head==null){
+			head=p;
+		}
+		else{
+			Node temp=head;
+			while(temp.next!=null){
+				temp=temp.next;
+			}
+			temp.next=p;
+		
+		}
+	
+	
+	
+	}
+	void display(){
+		Node temp=head;
+	        while(temp!=null){
+			System.out.print(temp.data+" --> ");
+			temp=temp.next;
+                
+		}	
+
+		System.out.print("null ");
+		System.out.println();
+
+
+	
+	}
+	boolean checkPalindrome(){
+		Stack<Integer> st=new Stack<>();
+		Node temp=head;
+		
+		while(temp!=null){
+			
+			st.push(temp.data);
+			temp=temp.next;
+		}
+		
+		temp=head;
+		while(temp!=null){
+			int ele=st.pop();
+			if(temp.data!=ele){
+				return false;
+			
+			}
+			temp=temp.next;
+		}
+
+		return true;
+	}
+
+}
+class Client12{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		LinkedList ll=new LinkedList();
+
+		System.out.println("Enter 1 if you want to create linked list : ");
+		int no=sc.nextInt();
+		char ch;
+		do{
+			System.out.println("Enter data : ");
+                	int data=sc.nextInt();
+			ll.createList(data);
+			System.out.println("Enter Y or y if you want to add node again: ");
+			ch=sc.next().charAt(0);
+		
+		
+		}
+		while(ch=='Y' || ch=='y');
+	
+	
+		System.out.print("Given Linked List : ");
+	 	ll.display();
+	 	System.out.println();
+			System.out.println("Linked List palindrome  : "+ll.checkPalindrome());
+                
+                
+	}
+}
+
