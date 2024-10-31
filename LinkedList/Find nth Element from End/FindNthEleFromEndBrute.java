79a4a8ab0c6ee63e6ff63e16a483c73efdcf437b
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-31 19:56:56 +0530

    BruteForce Solution

---
+++ LinkedList/Find nth Element from End/FindNthEleFromEndBrute.java
@@ -0,0 +1,98 @@
+import java.util.*;
+class Node{
+	int data;
+	Node next;
+	Node(){
+		next=null;
+	}
+
+
+
+
+}
+class LinkedList{
+	Node head=null;
+
+	void createList(int data){
+		Node p=new Node();
+		p.data=data;
+		if(head==null){
+			head=p;
+		}
+		else{
+			Node temp=head;
+			while(temp.next!=null){
+				temp=temp.next;
+			}
+			temp.next=p;
+		
+		}
+	
+	
+	
+	}
+	void display(){
+		Node temp=head;
+	        while(temp!=null){
+			System.out.print(temp.data+" --> ");
+			temp=temp.next;
+                
+		}	
+
+		System.out.print("null ");
+		System.out.println();
+
+
+	
+	}
+	int findNthEleFromEnd(int k){
+	
+		int count=0;
+		Node temp=head;
+		while(temp!=null){
+			count++;
+			temp=temp.next;
+		
+		}
+		count=count-k;
+		temp=head;
+		while(count>0){
+			temp=temp.next;
+			count--;
+		
+		}
+		return temp.data;
+		
+		
+	}
+}
+class Client4{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		LinkedList ll=new LinkedList();
+
+		System.out.println("Enter 1 if you want to create linked list : ");
+		int no=sc.nextInt();
+		char ch;
+		do{
+			System.out.println("Enter data : ");
+                	int data=sc.nextInt();
+			ll.createList(data);
+			System.out.println("Enter Y or y if you want to add node again: ");
+			ch=sc.next().charAt(0);
+		
+		
+		}
+		while(ch=='Y' || ch=='y');
+	
+	
+		System.out.print("Given Linked List : ");
+	 	ll.display();
+	 	System.out.println();
+		System.out.println("Enter value of k : ");
+		int k=sc.nextInt();
+		System.out.println("Nth Element from Last in Linked List : "+ ll.findNthEleFromEnd(k));
+                System.out.println();
+	}
+}
+
