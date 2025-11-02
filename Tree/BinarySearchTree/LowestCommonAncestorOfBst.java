49b72e0bd50e91d22225ac5c4b3d0402fdfb00c7
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2025-11-02 19:09:36 +0530

    Add files via upload

---
+++ Tree/BinarySearchTree/LowestCommonAncestorOfBst.java
@@ -0,0 +1,23 @@
+public class TreeNode {
+    int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null){
+            return root;
+        }
+        if(p.val<root.val && q.val<root.val){
+            return lowestCommonAncestor(root.left,p,q);
+        }
+        else if(p.val>root.val && root.val<q.val){
+            return lowestCommonAncestor(root.right,p,q);
+        }
+        else{
+            return root;
+        }    
+    }
+}
