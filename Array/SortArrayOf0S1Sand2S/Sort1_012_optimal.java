f14862cbf30b0a69a82fa97b5e3f37f4279599d6
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2026-01-21 14:40:06 +0530

    Update Sort1_012_optimal.java

--- Array/SortArrayOf0S1Sand2S/Sort1_012_optimal.java
+++ Array/SortArrayOf0S1Sand2S/Sort1_012_optimal.java
@@ -1,46 +1,50 @@
 import java.util.*;
-class Sort1{
-	static void Sort(int a[],int n){
-		for(int i=0;i<n;i++){
-			int temp=0,low=0,mid=0,high=n-1;
-                	if(a[i]==0){
-				temp=a[low];
-				a[low]=a[mid];
-				a[mid]=temp;
-				low++;
-				mid++;
-			}
-			else if(a[i]==1){
-                                mid++;
-                        }
-			else{
-				temp=a[high];
-                                a[high]=a[mid];
-                                a[mid]=temp;
-				high--;
-			}
-		}
-	}
-	static void Print(int arr[],int n){
-		 for(int i=0;i<n;i++){
-                        System.out.print(arr[i]+" ");
 
-                }
-	
-	}
-	public static void main(String[] args){
-		Scanner sc=new Scanner(System.in);
-		int n;
-		System.out.print("Enter number of  elements : ");
-		n=sc.nextInt();
-		int arr[]=new int[n];
-		System.out.print("Enter the elements : ");
-		for(int i=0;i<n;i++){
-			arr[i]=sc.nextInt();
-		
-		}
-		Sort(arr,n);
-		Print(arr,n);
-	
-	}
+class Sort1 {
+
+    static void Sort(int a[], int n) {
+        int low = 0, mid = 0, high = n - 1;
+        int temp;
+
+        while (mid <= high) {
+            if (a[mid] == 0) {
+                temp = a[low];
+                a[low] = a[mid];
+                a[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if (a[mid] == 1) {
+                mid++;
+            }
+            else { // a[mid] == 2
+                temp = a[mid];
+                a[mid] = a[high];
+                a[high] = temp;
+                high--;
+            }
+        }
+    }
+
+    static void Print(int arr[], int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.print("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Sort(arr, n);
+        Print(arr, n);
+    }
 }
