67674a78a76f77009e6c497bb05196949d20b6b8
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2026-01-26 14:42:27 +0530

    Create GreaterBinaryTree.java

---
+++ Tree/BinarySearchTree/GreaterBinaryTree.java
@@ -0,0 +1,16 @@
+class Solution {
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) return null;
+
+        convertBST(root.right);
+        sum += root.val;
+        root.val = sum;
+        convertBST(root.left);
+
+        return root;
+    }
+}
+// TC- O(n)
+//sc-O(h) height of bst
