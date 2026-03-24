3755b3fb7f4df9e2b6d2a5af3301d75659e1ed46
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2026-03-24 16:20:15 +0530

    Create BinaryTreeToGreaterSunTree.java

---
+++ Tree/BinaryTree/BinaryTreeToGreaterSunTree.java
@@ -0,0 +1,11 @@
+class BinaryTree {
+    int sum=0;
+    public TreeNode bstToGst(TreeNode root) {
+        if(root==null) return root;;
+        bstToGst(root.right);
+        sum+=root.val;
+        root.val=sum;
+        bstToGst(root.left);
+        return root;    
+    }
+}
