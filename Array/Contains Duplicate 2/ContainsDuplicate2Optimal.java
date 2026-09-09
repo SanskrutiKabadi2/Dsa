7d1941a83a9aafffcc87db9fa898eb424f60c113
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2026-09-09 07:49:18 +0530

    Create ContainsDuplicate2Optimal.java

---
+++ Array/Contains Duplicate 2/ContainsDuplicate2Optimal.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> st=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(i>k){
+                st.remove(nums[i-k-1]);
+            }
+            if(!st.add(nums[i])){
+                    return true;
+            }    
+        }
+        return false;
+    }
+}
